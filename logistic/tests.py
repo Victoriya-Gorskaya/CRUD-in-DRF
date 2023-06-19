11406d621409b121c0ecb2a582e3eeab68eeae5d
Victoriya Gorskaya <victoriya.gorskaya@mail.ru>
2023-06-19 10:15:58 +0300

    Add CI14

--- logistic/tests.py
+++ logistic/tests.py
@@ -4,13 +4,9 @@ from unittest import TestCase
 
 class SampleTestCase(TestCase):
     def test_successful_request(self):
+        client = APIClient()
         url = '/api/v1/test/'
-        client = APIClient()
         response = client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, "Hello world!")
 
-    def test_bad_case(self):
-        url = '/api/v1/sample/'
-        client = APIClient()
-        response = client.get(url)
-        self.assertEqual(response.status_code, 404)
+
--- logistic/views.py
+++ logistic/views.py
@@ -1,7 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 from django_filters.rest_framework import DjangoFilterBackend
